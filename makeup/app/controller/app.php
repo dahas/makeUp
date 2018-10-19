d973f140bd5914a1bed6447850ac86444c415088
dahas <dahas@gmx.de>
2018-10-19 19:21:52 +0200

    .

--- makeup/app/controller/app.php
+++ makeup/app/controller/app.php
@@ -46,40 +46,42 @@ class App extends Module
      */
     protected function build($module = "", $task = "") : string
     {
+        $m = [];
+        
         /**** IMPORTANT: Module with page content must come first! *************/
         if (!$task) {
-            $marker["##MODULES##"] = Module::create($module)->build();
+            $m["##MODULES##"] = Module::create($module)->build();
         } else {
-            $marker["##MODULES##"] = Module::create($module)->$task();
+            $m["##MODULES##"] = Module::create($module)->$task();
         }
 
         /**** Parsing the HTML head section ************************************/
 
-        $marker["##TITLE##"] = Template::createTitleTag();
-        $marker["##HTML_LANG##"] = Config::get("page_settings", "html_lang");
+        $m["##TITLE##"] = Template::createTitleTag();
+        $m["##HTML_LANG##"] = Config::get("page_settings", "html_lang");
 
-        $marker["##CONF_METATAGS##"] = Template::createMetaTags();
-        $marker["##CONF_CSS_FILES##"] = Template::createStylesheetTags();
-        $marker["##CONF_JS_FILES_HEAD##"] = Template::createJsScriptTagsHead();
+        $m["##CONF_METATAGS##"] = Template::createMetaTags();
+        $m["##CONF_CSS_FILES##"] = Template::createStylesheetTags();
+        $m["##CONF_JS_FILES_HEAD##"] = Template::createJsScriptTagsHead();
         
-        $marker["##COOKIE_NAME##"] = Config::get("cookie", "name") ?: "makeup";
-        $marker["##COOKIE_EXP##"] = Config::get("cookie", "expires_days") ?: 0;
-        $marker["##COOKIE_PATH##"] = Config::get("cookie", "path") ?: "/";
+        $m["##COOKIE_NAME##"] = Config::get("cookie", "name") ?: "makeup";
+        $m["##COOKIE_EXP##"] = Config::get("cookie", "expires_days") ?: 0;
+        $m["##COOKIE_PATH##"] = Config::get("cookie", "path") ?: "/";
 
-        $marker["##CONF_JS_FILES_BODY##"] = Template::createJsScriptTagsBody();
+        $m["##CONF_JS_FILES_BODY##"] = Template::createJsScriptTagsBody();
         
-        $marker["##LANG_TITLE##"] = Lang::get("title");
-        $marker["##LANG_SUBTITLE##"] = Lang::get("subtitle");
-        $marker["##LANG_WIKI_BUTTON##"] = Lang::get("wiki_button");
+        $m["##LANG_TITLE##"] = Lang::get("title");
+        $m["##LANG_SUBTITLE##"] = Lang::get("subtitle");
+        $m["##LANG_WIKI_BUTTON##"] = Lang::get("wiki_button");
 
         /**** Parsing the HTML body section ************************************/
 
-        $marker["##NAVIGATION##"] = Module::create("navigation")->build(); // Connecting the menu to the navbar
-        $marker["##LOGIN##"] = Module::create("login")->build("nav"); // Connecting the login form to the navbar
-        $marker["##LANGUAGE_SELECTOR##"] = Module::create("language_selector")->build(); // Connecting the language selector
-        $marker["##SUBTITLE##"] = Config::get("page_settings", "subtitle");
+        $m["##NAVIGATION##"] = Module::create("navigation")->build(); // Connecting the menu to the navbar
+        $m["##LOGIN##"] = Module::create("login")->build("nav"); // Connecting the login form to the navbar
+        $m["##LANGUAGE_SELECTOR##"] = Module::create("language_selector")->build(); // Connecting the language selector
+        $m["##SUBTITLE##"] = Config::get("page_settings", "subtitle");
 
-        return $this->getTemplate()->parse($marker);
+        return $this->getTemplate()->parse($m);
     }
     
 }      
