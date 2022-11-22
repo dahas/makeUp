e8f307a309c4c9bfb52d1771eb85a0f6c4be19d2
daHas <dahas@gmx.de>
2022-11-22 18:00:54 +0100

    v2.2

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 
-error_reporting(E_ALL);
+/**
+ * IMPORTANT: Set error reporting to 0 before deploying to production!
+ */
+error_reporting(E_ALL); // error_reporting(0);
 
 require_once(str_replace("public", "", __DIR__) . 'makeup/app/controller/app.php');
 $App = new makeup\app\controller\App();
