bf235ecddf04f4aa2b91dbebe65b02308830181c
daHas <dahas@gmx.de>
2022-12-23 14:22:47 +0100

    .

--- public/index.php
+++ public/index.php
@@ -5,21 +5,11 @@ use makeUp\lib\Router;
 
 require dirname(__DIR__, 1) . "/makeup/vendor/autoload.php";
 
-// if (isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] === "localhost:2400" || $_SERVER['HTTP_HOST'] === "makeup.loc")) {
+if (isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] === "localhost:2400")) {
     error_reporting(E_ALL);
-// } else {
-//     error_reporting(0);
-// }
-
-## SET FOR DEBUGGER. COMMENT OUT IN BROWSER! ##
-## ----------------------------------------- ##
-if (isset($_SERVER['argc']) && $_SERVER['argc'] > 0) {
-    $_SERVER['REQUEST_URI'] = "/test?task=delete&uid=3";
-    // $_SERVER['REQUEST_URI'] = "/index";
-    $_SERVER['REQUEST_METHOD'] = "GET";
-    $_POST['user'] = "Test data";
+} else {
+    error_reporting(0);
 }
-## ----------------------------------------- ##
 
 $Router = new Router();
 $Router->get("/", [App::class, "execute"])->run();
