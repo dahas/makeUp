9e777cd29538d42675d3009d5d93c32522769c91
dahas <martin.j.wolf@gmail.com>
2023-01-12 10:20:00 +0100

    Event handling optimized

--- public/resources/js/TopModels.js
+++ public/resources/js/TopModels.js
@@ -1,22 +1,14 @@
 
 $(document).ready(() => {
 
-    /*****************************************************************************\
-    | IMPORTANT NOTE: The events of the Bootstrap collapsible must be registered  |
-    | on document level because elements are asynchronously inserted and removed  |
-    | from the DOM.                                                               |
-    \*****************************************************************************/
-
     $(document).on('show.bs.collapse', event => {
         if (event.target.id == "collapseOne") {
-            console.log("EXPANDING");
             Cookie.set("collapseOne_expanded", true)
         }
     })
 
     $(document).on('hide.bs.collapse', event => {
         if (event.target.id == "collapseOne") {
-            console.log("COLLAPSING");
             Cookie.set("collapseOne_expanded", false)
         }
     })
