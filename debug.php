c23e4514efd16772e8dfe9ac20c14a43c7a3edeb
dahas <martin.j.wolf@gmail.com>
2023-03-12 16:52:00 +0100

    .

--- debug.php
+++ debug.php
@@ -9,9 +9,9 @@ error_reporting(E_ALL);
 
 $_SERVER['REQUEST_METHOD'] = "GET";
 // $_SERVER['REQUEST_URI'] = "/SampleData/getItem?uid=6";
-// $_SERVER['REQUEST_URI'] = "/Home";
-$_SERVER['REQUEST_URI'] = "/Authentication/buildLogoutForm";
-$_SERVER['HTTP_X_MAKEUP_AJAX'] = 1;
+$_SERVER['REQUEST_URI'] = "/Home";
+// $_SERVER['REQUEST_URI'] = "/Authentication/buildLogoutForm";
+// $_SERVER['HTTP_X_MAKEUP_AJAX'] = 1;
 
 // $_POST['param1'] = "Post data 1";
 
